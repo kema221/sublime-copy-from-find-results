8e59abef4c7e2fa824984e779f79c267599fa920
kema221 <kema221@gmail.com>
2015-07-14 16:12:52 +0800

    Update copy_from_find_results_command.py
    Add RegexStruct2 to remove list file name

--- copy_from_find_results_command.py
+++ copy_from_find_results_command.py
@@ -15,8 +15,9 @@ class CopyFromFindResultsCommand(sublime_plugin.TextCommand):
         if clipboard_contents:
             settings = sublime.load_settings('CopyFromFindResults.sublime-settings')
             keep_intermediate_dots = settings.get('keep_intermediate_dots', False)
-            new_clipboard = RegexStruct(keep_intermediate_dots).sub(clipboard_contents)
-            sublime.set_clipboard(new_clipboard)
+            new_clipboard = RegexStruct2(keep_intermediate_dots).sub(clipboard_contents)
+            new_clipboard2 = RegexStruct2(keep_intermediate_dots).sub(new_clipboard)
+            sublime.set_clipboard(new_clipboard2)
 
     def in_find_results_view(self):
         return self.view.settings().get('syntax') == 'Packages/Default/Find Results.hidden-tmLanguage'
@@ -34,3 +35,15 @@ class RegexStruct():
 
     def construct(self):
         return RegexStruct.default if self.keep_dots else RegexStruct.without_dots
+class RegexStruct2():
+    default = re.compile('^\s*\S+(\:\s|\s{2})', re.MULTILINE)
+    without_dots = re.compile('^\s*(\S+(\:\s|\s{2})|\.+)', re.MULTILINE)
+
+    def __init__(self, keep_dots=True):
+        self.keep_dots = keep_dots
+
+    def sub(self, text):
+        return self.construct().sub('', text)
+
+    def construct(self):
+        return RegexStruct2.default if self.keep_dots else RegexStruct2.without_dots
